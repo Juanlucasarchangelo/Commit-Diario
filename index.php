fa6f866fd884d2e265b109b0882508926a2eb7d2
juanlucasarchangelo <jlarchangelo@outlook.com>
2023-07-03 12:31:16 -0300

    Estou trabalhando  - 03/07/2023

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $dataAtual = date("d/m/Y");
 
 // Crie a mensagem de commit com a data atual
-$mensagemCommit = "Estou trabalhando - " . $dataAtual;
+$mensagemCommit = "Estou trabalhando  - " . $dataAtual;
 
 // Adicione todos os arquivos alterados
 exec("git add .");
